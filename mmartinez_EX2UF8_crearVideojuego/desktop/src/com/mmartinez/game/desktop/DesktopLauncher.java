7a74114054237b1c013d53f168989daaa78c0d89
Marc Martinez <calored11@gmail.com>
2020-02-03 17:14:10 +0100

    commit final
    juego snake

---
+++ mmartinez_EX2UF8_crearVideojuego/desktop/src/com/mmartinez/game/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.mmartinez.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mmartinez.game.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 402;
+		config.height = 658;
+		new LwjglApplication(new Main(), config);
+	}
+}
